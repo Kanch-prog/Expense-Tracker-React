9f816131d4944165a789a267c1168fea42426667
Kanch-prog <121807277+Kanch-prog@users.noreply.github.com>
2023-07-07 05:41:31 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+import App from "./App";
+
+import { createRoot } from "react-dom/client";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
